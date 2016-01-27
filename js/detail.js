ba4204da14860818b079fd4a3d830c741df3f416
wgx <wgx998877@qq.com>
2016-01-28 01:39:56 +0800

    wgx

---
+++ js/detail.js
@@ -0,0 +1,15 @@
+$(function () {
+  var server_url = '';
+  //getBrandList();
+
+  function getBarndList(){
+  	$.ajax({
+  		'url':server_url,
+  		success:function(data){
+  			
+  		}
+  	});
+  }
+
+  $.init();
+});
